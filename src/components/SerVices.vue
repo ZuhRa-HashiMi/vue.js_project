7bccf0d3fed5ca142d5e422715ad91226e5553cc
Zuhra Hashimi <zuhra.tactaz@yahoo.com>
2022-10-30 23:58:07 +0430

    maked services section

--- src/components/SerVices.vue
+++ src/components/SerVices.vue
@@ -1,19 +1,13 @@
 <template>
 
-<!-- Main Program Section -->
-
 <div class="services">
     
     <div class="container">
-      <div class="text-wrap">
-      <h3>Services</h3>
-    </div>
-      <div class="text-wrap">
+      <h3>Services</h3>>
         <h4>Creating and deploying software that ensure growth in business.</h4>
-    </div>
-      <div class="row text-center">
-        <div class="col-md-3">
-          <div><i class="fas fa-globe"></i></div>
+      <div class="row">
+        <div class="card">
+          <div><i class="fas fa-globe fa-5x"></i></div>
           <div class="serv">
             <h5>Business Applications</h5>
             <p>Are you looking to connect your people, automate processes, or improve productivity to enable growth and competitive advantage? We’ve got you covered.
@@ -33,18 +27,17 @@
                 </ol></p>
           </div>
         </div>
-        </div>
        
-        <div class="col-md-3">
+        <div class="card">
           <div class="serv">
-            <div><i class="fas fa-cogs"></i></div>
+            <div><i class="fas fa-cogs fa-5x"></i></div>
             <h5>Web Development</h5>
             <p>State-of-the-art web applications.
                 We’re experts at building solid, reliable, and fast web applications from the ground up.</p>
           </div>
         </div>
-        <div class="col-md-3">
-          <div><i class="fas fa-globe"></i></div>
+        <div class="card">
+          <div><i class="fas fa-globe fa-5x"></i></div>
           <div class="serv">
             <h5>Mobile App Development</h5>
             <p>Delightful iOS and Android apps.
@@ -66,12 +59,12 @@
                 </ol></p>
           </div>
         </div>
-        <div class="col-md-3">
-          <div><i class="fas fa-user-shield"></i></div>
+        <div class="card">
+          <div><i class="fas fa-user-shield fa-5x"></i></div>
           <div class="serv">
             <h5>Product Design and Management</h5>
             <p>Our expertise in Product Design & Product Management is backed by in-depth research and understanding of your industry.
-                That’s how we deliver beautiful and functional products.
+                That’s how we deliver beautiful and functional products.  </p>
 
                 <ol>
                     <li>
@@ -90,11 +83,12 @@
                         Research
                     </li>
                 </ol>
-           </p>
+         
           </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>

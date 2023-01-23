d030a5dec3d08af70072bd11cb18ca69b0295dbe
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-23 17:40:59 +0430

    add all the files

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,101 @@
 <template>
-<div id="App">
-    <NavBar />
-    <JumBotron />
-    <SerVices />
-    <MyFooter />
-</div>
+  <div id="app">
+    <NavigationMobile />
+    <div class="content" :class="{'open':showNav}">
+      <div class="top-bar">
+        <div id="navigation-icon" v-if="mobileView"
+          @click="showNav = !showNav">
+          <i class="fas fa-bars"></i>
+        </div>
+        <NavigationBar v-if="!mobileView" />
+      </div>
+      <ContentPage />
+    </div>
+  </div>
 </template>
 
 <script>
+import NavigationBar from "./components/NavigationBar.vue";
+import NavigationMobile from "./components/NavigationMobile.vue";
+import ContentPage from "./components/ContentPage.vue";
 
-import NavBar from './components/NavBar.vue';
-import JumBotron from './components/jumBotron.vue';
-import SerVices from './components/SerVices.vue';
-import MyFooter from './components/MyFooter.vue';
 export default {
-    name: "App",
-    components: {
-    NavBar,
-    JumBotron,
-    SerVices,
-    MyFooter
+  data: () => {
+    return {
+      mobileView: true,
+      showNav: false
+    };
+  },
+  methods: {
+    handleView() {
+      this.mobileView = window.innerWidth <= 990;
+    }
+  },
+  components: {
+    NavigationBar,
+    NavigationMobile,
+    ContentPage
 },
+  created() {
+    this.handleView();
+    window.addEventListener('resize', this.handleView);
+  }
 };
 </script>
 
 <style>
+@import url("https://use.fontawesome.com/releases/v5.9.0/css/all.css");
+
+* {
+  font-size: 1rem;
+}
+
 body {
-    padding: 0;
-    margin: 0;
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  font-family: "Segoe UI", Tahoma;
+  background-color: #7ca971;
+}
+
+#app {
+  position: relative;
+  width: calc(100% - 20px);
+  height: calc(100vh - 20px);
+  padding: 10px;
+  color: #333;
+  overflow: hidden;
+}
+
+.top-bar {
+  display: flex;
+  width: 100%;
+}
+
+#navigation-icon {
+  padding: 10px 10px 20px;
+  margin-right: 10px;
+  cursor: pointer;
+
+  i {
+    font-size: 2rem;
+  }
+}
+
+.content {
+  position: absolute;
+  top: 10px;
+  width: calc(100% - 60px);
+  height: calc(100vh - 60px);
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 30px;
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  transition: 1s transform cubic-bezier(0,.12,.14,1);
+}
+
+.open {
+  transform: translateX(300px);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,23 +1,8 @@
-
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.css"
-import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
-// import './assets/script.js'
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
 
-/* import font awesome icon component */
-// eslint-disable-next-line no-unused-vars
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+Vue.config.productionTip = false
 
-/* import specific icons */
-import { fas } from '@fortawesome/free-solid-svg-icons'
-
-/* add icons to the library */
-library.add(fas);
-
-createApp(App).use(bootstrap) 
- .component('fa', FontAwesomeIcon)
- .mount('#app');
-
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
